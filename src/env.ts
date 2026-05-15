f965ca1b6a5887267c593c685daecaa5aee7424a
jeondabeen <jeondabeen29@gmail.com>
2026-05-15 16:18:15 +0900

    Feat: env.ts 추가

---
+++ src/env.ts
@@ -0,0 +1,5 @@
+export const ENV = {
+  KAKAO_API_KEY: import.meta.env.VITE_KAKAO_API_KEY,
+  TOUR_API_KEY: import.meta.env.VITE_TOUR_API_KEY,
+  TOUR_OPEN_API_URL: import.meta.env.VITE_TOUR_OPEN_API_URL,
+};
