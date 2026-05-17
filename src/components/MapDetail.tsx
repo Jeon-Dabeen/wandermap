b7cecbe95e4752f518a28e4438564279a7182504
jan <jeondabeen29@gmail.com>
2026-05-17 11:10:44 +0900

    Feat: 상태 위치 설계 변경

--- src/components/MapDetail.tsx
+++ src/components/MapDetail.tsx
@@ -1,23 +1,11 @@
-import PositionContext from "../contexts/PositionContext";
-import type { Coordinates } from "../types";
 import ResetPositionButton from "./ResetPositionButton";
 import TourMap from "./TourMap";
 
-import { useState } from "react";
-
 export default function MapDetail() {
-  const [currentPosition, setCurrentPosition] = useState<Coordinates>();
-
-  const changePosition = (coordinates: Coordinates) => {
-    setCurrentPosition(coordinates);
-  };
-
   return (
     <div>
-      <PositionContext.Provider value={{ changePosition, currentPosition }}>
-        <TourMap />
-        <ResetPositionButton />
-      </PositionContext.Provider>
+      <TourMap />
+      <ResetPositionButton />
     </div>
   );
 }
